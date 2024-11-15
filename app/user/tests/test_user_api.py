b3ce3858d4adb831e9c19082638aaeddbbf78e0c
GH <chelsey.gahyun@gmail.com>
2024-11-15 20:54:11 +0900

    added health check api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -51,7 +51,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_password_too_chort_error(self):
+    def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
         payload = {
             'email': 'test@example.com',
@@ -90,6 +90,14 @@ class PublicUserApiTests(TestCase):
         create_user(email='test@example.com', password='goodpass')
 
         payload = {'email': 'test@example.com', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
